9f363f8327ffe763642062e3ba57121b014c40aa
eunji <moeunji12@gmail.com>
2025-01-22 18:48:06 +0900

    day1

---
+++ day1/숫자형.py
@@ -0,0 +1,20 @@
+num1 = 10 # 양의 정수
+num2 = -3 # 음의 정수
+num3 = 0
+print(num1)
+print(num2)
+print(num3)
+
+print(num1 - num2)
+
+num1 = 100
+num3 = 20
+print(num1 - num3)
+
+# int()
+print(int(1.9))
+print(int('100'))
+print(int(True)) # 1을 반환
+print(int(False)) # 0을 반환
+
+print(int(True + 10))
---
+++ day1/신상정보.py
@@ -0,0 +1,16 @@
+name = '모은지' # 싱글라인으로 (한줄)문자열을 저장하는 변수
+age = 20
+address = '''
+우편번호 112233
+서울특별시 강남구 신사동
+신사아파트 5002호
+'''
+# 멀티플라인으로 (여러줄 문자열 데이터를 작성할 때)
+weight = 50
+height = 180
+
+print(name)
+print(age)
+print(address)
+print(weight)
+print(height)
